b1c52120bd703679cdec99a17074459cc7e98d3a
Vincenzo Mackjula <vmackland@gmail.com>
2025-08-14 00:15:26 -0400

    Save homepage neon image changes before pull

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
-
 import '../styles/globals.css'
+import Image from 'next/image'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <header className="header">
-        <div className="brand">THE GREEN ROOM</div>
+        <div className="brand">
+          <Image src="/logo.png" alt="The Green Room" width={36} height={36} priority />
+          <span>THE GREEN ROOM</span>
+        </div>
         <nav className="nav">
           <a href="/">Home</a>
           <a href="/membership">Membership</a>
-          <a href="/reservations">Reservations</a>
+          <a href="/reservations">Café Reservations</a>
+          <a href="/after-hours">After Hours Entry</a>
           <a href="/menu">Order</a>
           <a href="/profile">Profile</a>
           <a href="/admin">Admin</a>
